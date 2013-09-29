20c918eee9e9bb1061afb1ba57c928de092990ff
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-09-29 10:30:45 +0200

    Use appdmg_eula provider and link to download site

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,8 +4,10 @@
 #
 #  include omnidisksweeper
 class omnidisksweeper {
+
   package { 'OmniDiskSweeper':
-    provider => 'appdmg',
-    source   => 'https://yum.cegeka.be/osx_packages/OmniDiskSweeper-1.8.dmg',
+    provider => 'appdmg_eula',
+    source   => 'http://downloads2.omnigroup.com/software/MacOSX/10.6/OmniDiskSweeper-1.8.dmg',
   }
+
 }
--- spec/classes/omnidisksweeper_spec.rb
+++ spec/classes/omnidisksweeper_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'omnidisksweeper' do
 
   it { should contain_class('omnidisksweeper') }
-  it { should contain_package('OmniDiskSweeper').with_provider('appdmg') }
-  it { should contain_package('OmniDiskSweeper').with_source('https://yum.cegeka.be/osx_packages/OmniDiskSweeper-1.8.dmg') }
+  it { should contain_package('OmniDiskSweeper').with_provider('appdmg_eula') }
+  it { should contain_package('OmniDiskSweeper').with_source('http://downloads2.omnigroup.com/software/MacOSX/10.6/OmniDiskSweeper-1.8.dmg') }
 
 end
